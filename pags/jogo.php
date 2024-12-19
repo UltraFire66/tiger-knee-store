063bca51354d2b36fd1278bad9ac88f32d17c228
guilhermef21 <145135647+guilhermef21@users.noreply.github.com>
2024-12-19 12:03:21 -0300

    Add files via upload

---
+++ pags/jogo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet"  href="../styles/teste.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jogo</title>
+</head>
+<body >
+    <div id="cabecalho">
+        <img src="../figures/silenthill2.png" alt="Cabeçalho" width="10%" height="10%">
+    </div>
+    <div class="img1-sinopse">
+        <div class ="img1">
+            <img src="../figures/silenthill2.png" alt="Imagem à esquerda" width="30%" height="30%">
+            <img src="../figures/rate.png" alt="Imagem à esquerda" width="30%" height="30%">
+        <div>
+        <div class="sinopse">
+            KKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+        </div>
+    </div>
+    
+</body>
+</html>
