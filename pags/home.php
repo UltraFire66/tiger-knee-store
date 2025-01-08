01d1afa9e14f16d2136e6a4393248e526caba84c
Caio Rangel <71445980+UltraFire66@users.noreply.github.com>
2025-01-08 10:26:08 -0300

    inicio da pag de jogo

--- pags/home.php
+++ pags/home.php
@@ -13,122 +13,12 @@
     $dados = mysqli_fetch_array($resultado);
     mysqli_close($connect);
 ?>
-
-<?php
-
- $ids = array("1126190","39140","813780","366250","254460","587620","12200","323470", "220");
-
- $jogos = array();
-  foreach ($ids as $id){
-    $link = 'https://store.steampowered.com/api/appdetails?appids='.$id;
-    $jogos[] = json_decode(file_get_contents($link),true);
-  }
-
-
-
-
-?>
-
-
-
 <html>
     <head>
         <title>Página restrita</title>
-        <link rel="stylesheet"  href="../estilos/home.css" />
     </head>
-    <body style = "max-width: 100vw">
-        
-        <div style = "background-color: #8C0005;
-        width: 100%;
-        height: 17vh;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between" >
-
-            <div style = "display:flex; flex-direction: row">
-                <div style = "font-size: 30px; color: #9EA407; margin-left: 15px; display:flex; align-items: center; justify-content: center; flex-direction: column; width: 10vw; height: 100%;">
-                        <p style = "margin: 0; padding: 5px;">TYGER</p>
-                        <p style = "margin: 0 0 0 40px; padding: 5px;">KNEE</p>
-                        <p style = "margin: 0; padding: 5px;">STORE</p>
-                </div>
-                    <img style="width: 5vw; height: 10vh; margin-left: 20px; align-self: center" src="../figures/logo.gif" alt=""/>
-            </div>
-            
-
-            <p style = "align-self: end; margin: 10px; font-size: 30px">Gêneros</p>
-            <p style = "align-self: end; margin: 10px; font-size: 30px">Lançamento</p>
-            <p style = "align-self: end; margin: 10px; font-size: 30px">Bem avaliados</p>
-            <div style = "display: flex;flex-direction: column; margin-right: 25px">
-                <img style="width: 5vw; height: 10vh; align-self: center; justify-self: center" src="../figures/perfil.png" alt=""/>
-                <p style = " font-size: 20px; align-self: center">Caio</p>
-            </div>
-
-        </div>
-
-        <div style = "display: flex; flex-direction: row">
-            <div style = "background-color: #A9A17A;  height: 83vh; width: 17vw; display: flex; justify-content: center; font-size: 30px">
-                <p> Filtros</p>
-            </div>
-            <div style="display: flex; flex-direction: column" >
-                <div style = "width = 83vw , height: 8vh; display: flex; align-items: center; justify-content: center; background-color: green"> 
-                    <input type="text" id = "pesquisar" style = "border: 10px; border-radius: 5px; border-color: black">
-                </div>
-                <div style = "width = 83vw , height: 83vh , background-color: blue ; display: grid; grid-template-columns: auto auto auto auto auto">
-                    <?php
-                    $pos = 0;
-                    foreach($jogos as  $jogo){
-                        $posicao = $ids[$pos];
-                        $imagem_capa = $jogo[$posicao]["data"]["header_image"];
-                        $nome = $jogo[$posicao]["data"]["name"];
-                        $genero = $jogo[$posicao]["data"]["genres"][0]["description"];
-                        //parafernalha pra colocar a virgula no preco
-                        $preco_nao_formatado = $jogo[$posicao]["data"]["price_overview"]["initial"];;
-                        $p = str_split($preco_nao_formatado);
-                        $p[sizeof($p)] = $p[sizeof($p) - 1];
-                        $p[sizeof($p) - 2] = $p[sizeof($p) - 3];
-                        $p[sizeof($p) - 3] = ",";
-                        $preco = implode("",$p);
-                        $pos = $pos + 1;
-
-                    echo <<<card
-                    <div class="card" style = "
-                        display: flex;
-                        justify-content: center;
-
-                        margin-top: 4vh;
-                        margin-left: 3vw;
-
-                        flex-direction: column;
-                        height: 28.5vh;
-                        width: 12vw;
-                        box-shadow: 10px 10px 5px gray;
-
-                        ">
-                            <img class = "imagem-capa" style = "width: auto; height: auto" src = $imagem_capa>
-                            
-                                <div style = "margin-left: 15px">
-                                    <p style = "font-size: 15px; align-self: center; margin-top: 3px"> $nome</p>
-                                    <p style = "font-size: 10px;  align-self: center; margin-top: -14px">  $genero</p>
-                                </div>
-                                
-                                <img style = "width: 8vw; height: 5vh; align-self: center; margin-top: -7px; margin-bottom: 7px" class = "imagem" src = "../figures/rate.png">
-                    
-                            
-                            <div style = "background-color: green; width: 100%; height: auto; display: flex; justify-content: center">
-                            <p style = "font-size: 25px; margin-top: 3px; margin-bottom: 3px; "> R$ $preco</p>
-                            </div>
-                            
-                        </div>
-                    card;
-                    }
-                    
-                    ?>
-                </div>
-            </div>
-            
-        </div>
-    
-
-
+    <body>
+        <h1 >Olá <?php echo $dados ['nome']; ?></h1 >
+        <a href ="../index.php ">Sair</a>
     </body>
 </html>
--- pags/jogo.php
+++ pags/jogo.php
@@ -2,21 +2,58 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet"  href="../estilos/teste.css"/>
+    <link rel="stylesheet"  href="../styles/teste.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
     <title>Jogo</title>
 </head>
-<body >
-    <div id="cabecalho">
+<body style="background-color: #fcf7d1">
+    <div id="cabecalho" >
         <img src="../figures/silenthill2.png" alt="Cabeçalho" width="10%" height="10%">
     </div>
     <div class="img1-sinopse">
-        <div class ="img1">
-            <img src="../figures/silenthill2.png" alt="Imagem à esquerda" width="30%" height="30%">
-            <img src="../figures/rate.png" alt="Imagem à esquerda" width="30%" height="30%">
-        <div>
-        <div class="sinopse">
-            KKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+        <div class ="img1" style="min-width: 30%; min-height: 30%; margin-left: 10vw; ">
+            <img src="../figures/silenthill2.png" alt="Imagem à esquerda" width="70%" height="70%">
+            <img src="../figures/rate.png" alt="Imagem à esquerda" width="70%" height="70%">
+        </div>
+        <div class="sinopse" style="margin-top: 10vh; margin-left: 0vw; margin-right: 10vw; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+        <p style="font-size:20px">Vivencie uma verdadeira aula de terror psicológico com este jogo, aclamado como o melhor da série, no hardware mais atual com visuais sinistros e sons de arrepiar.
+
+Assuma o papel de James Sunderland e explore a quase deserta cidade de Silent Hill neste muito aguardado remake do clássico de 2001. Atraído até este lugar misterioso por uma carta de sua esposa, que morreu há três anos, James vasculha a cidade atrás de vestígios dela.
+
+Entre num mundo onírico e se depare com monstros perversos, o ameaçador Pyramid Head e um elenco de personagens aparentemente comuns que tentam lidar com o passado.
+
+À medida que James aceita sua instabilidade, uma pergunta ainda restará: qual o verdadeiro motivo para ele vir a Silent Hill?</p>
+        </div>
+    </div>
+    <div class="carrossel" style = "display: flex; justify-content: center; margin-top: 10vh">
+        <div id="carouselExampleIndicators" class="carousel slide" style = "width: 30%; height: 30%;">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="../figures/silenthill2.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="../figures/silenthill2.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="../figures/silenthill2.png" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
         </div>
     </div>
     
