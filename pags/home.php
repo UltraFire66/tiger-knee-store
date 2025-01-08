3d6712232165e560ccdcae44f1d033d9161ab3e2
Caio Rangel <71445980+UltraFire66@users.noreply.github.com>
2025-01-08 10:19:39 -0300

    inicio da barra de pesquisa

--- pags/home.php
+++ pags/home.php
@@ -16,7 +16,7 @@
 
 <?php
 
- $ids = array("1126190","39140","813780","366250","254460","587620","12200");
+ $ids = array("1126190","39140","813780","366250","254460","587620","12200","323470", "220");
 
  $jogos = array();
   foreach ($ids as $id){
@@ -69,51 +69,63 @@
             <div style = "background-color: #A9A17A;  height: 83vh; width: 17vw; display: flex; justify-content: center; font-size: 30px">
                 <p> Filtros</p>
             </div>
-            <div style = "width = 83vw , height: 83vh , background-color: blue ; display: grid; grid-template-columns: auto auto auto auto">
-                <?php
-                $pos = 0;
-                foreach($jogos as  $jogo){
-                    $posicao = $ids[$pos];
-                    $imagem_capa = $jogo[$posicao]["data"]["header_image"];
-                    $nome = $jogo[$posicao]["data"]["name"];
-                    $genero = $jogo[$posicao]["data"]["genres"][0]["description"];
-                    $preco = $jogo[$posicao]["data"]["price_overview"]["initial_formatted"];
-                    $pos = $pos + 1;
+            <div style="display: flex; flex-direction: column" >
+                <div style = "width = 83vw , height: 8vh; display: flex; align-items: center; justify-content: center; background-color: green"> 
+                    <input type="text" id = "pesquisar" style = "border: 10px; border-radius: 5px; border-color: black">
+                </div>
+                <div style = "width = 83vw , height: 83vh , background-color: blue ; display: grid; grid-template-columns: auto auto auto auto auto">
+                    <?php
+                    $pos = 0;
+                    foreach($jogos as  $jogo){
+                        $posicao = $ids[$pos];
+                        $imagem_capa = $jogo[$posicao]["data"]["header_image"];
+                        $nome = $jogo[$posicao]["data"]["name"];
+                        $genero = $jogo[$posicao]["data"]["genres"][0]["description"];
+                        //parafernalha pra colocar a virgula no preco
+                        $preco_nao_formatado = $jogo[$posicao]["data"]["price_overview"]["initial"];;
+                        $p = str_split($preco_nao_formatado);
+                        $p[sizeof($p)] = $p[sizeof($p) - 1];
+                        $p[sizeof($p) - 2] = $p[sizeof($p) - 3];
+                        $p[sizeof($p) - 3] = ",";
+                        $preco = implode("",$p);
+                        $pos = $pos + 1;
 
-                echo <<<card
-                <div class="card" style = "
-                    display: flex;
-                    justify-content: center;
+                    echo <<<card
+                    <div class="card" style = "
+                        display: flex;
+                        justify-content: center;
 
-                    margin-top: 4vh;
-                    margin-left: 3vw;
+                        margin-top: 4vh;
+                        margin-left: 3vw;
 
-                    flex-direction: column;
-                    height: 28.5vh;
-                    width: 12vw;
-                    box-shadow: 10px 10px 5px gray;
+                        flex-direction: column;
+                        height: 28.5vh;
+                        width: 12vw;
+                        box-shadow: 10px 10px 5px gray;
 
-                    ">
-                        <img class = "imagem-capa" style = "width: auto; height: auto" src = $imagem_capa>
-                        
-                            <div style = "margin-left: 15px">
-                                <p style = "font-size: 25px; align-self: center; margin-top: 3px"> $nome</p>
-                                <p style = "font-size: 20px;  align-self: center; margin-top: -14px">  $genero</p>
+                        ">
+                            <img class = "imagem-capa" style = "width: auto; height: auto" src = $imagem_capa>
+                            
+                                <div style = "margin-left: 15px">
+                                    <p style = "font-size: 15px; align-self: center; margin-top: 3px"> $nome</p>
+                                    <p style = "font-size: 10px;  align-self: center; margin-top: -14px">  $genero</p>
+                                </div>
+                                
+                                <img style = "width: 8vw; height: 5vh; align-self: center; margin-top: -7px; margin-bottom: 7px" class = "imagem" src = "../figures/rate.png">
+                    
+                            
+                            <div style = "background-color: green; width: 100%; height: auto; display: flex; justify-content: center">
+                            <p style = "font-size: 25px; margin-top: 3px; margin-bottom: 3px; "> R$ $preco</p>
                             </div>
                             
-                            <img style = "width: 8vw; height: 5vh; align-self: center; margin-top: -7px; margin-bottom: 7px" class = "imagem" src = "../figures/rate.png">
-                
-                        
-                        <div style = "background-color: green; width: 100%; height: auto; display: flex; justify-content: center">
-                        <p style = "font-size: 25px; margin-top: 3px; margin-bottom: 3px; "> $preco</p>
                         </div>
-                        
-                    </div>
-                card;
-                }
-                
-                ?>
+                    card;
+                    }
+                    
+                    ?>
+                </div>
             </div>
+            
         </div>
     
 
